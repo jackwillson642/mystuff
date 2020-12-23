9ca07f87bc18c08b424b6fd04e0e14505fd6383e
jackwillson642 <jackwillson642@gmail.com>
2020-12-23 19:27:21 +0530

    updates

--- .vimrc
+++ .vimrc
@@ -1,17 +1,18 @@
-"+--------------------------------+
-"|           _                    |
-"|   __   __(_)___ ___ ___  ____  |
-"|   | | / / / __ `__ \  _|/ __|  |
-"|   | |/ / / / / / / / / | |__   |
-"|   |___/_/_/ /_/ /_/_/   \___|  |
-"|                                |
-"|                                |
-"+--------------------------------+
+"+-------------------------------+
+"|          _                    |
+"|  __   __(_)___ ___ ___  ____  |
+"|  | | / / / __ `__ \  _|/ __|  |
+"|  | |/ / / / / / / / / | |__   |
+"|  |___/_/_/ /_/ /_/_/   \___|  |
+"|                               |
+"|                               |
+"+-------------------------------+
 
 call plug#begin('~/.vim/plugged')
 " Make sure you use single quotes
 
 Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
+Plug 'Valloric/YouCompleteMe'
 Plug 'tpope/vim-fireplace', { 'for': 'clojure' }
 Plug 'dracula/vim', { 'as': 'dracula' }
 
@@ -19,9 +20,13 @@ Plug 'dracula/vim', { 'as': 'dracula' }
 call plug#end()
 
 
+" Mappings
+set nocompatible
+
 " Line numbers and relative line number
 set number
 set relativenumber
+set numberwidth=6
 nnoremap <F1> :set number!<cr>:set relativenumber!<cr>
 inoremap <F1> <ESC>:set number!<cr>:set relativenumber!<cr>i
 
@@ -38,20 +43,16 @@ filetype plugin indent on
 
 " NERDTree
 autocmd StdinReadPre * let s:std_in=1
+" uncommenting line below will launch nerdtree when vim is launched
 autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
-nnoremap <F3> :NERDTreeToggle<cr>
-inoremap <F3> :NERDTreeToggle<cr>
 let NERDTreeWinSize=40
 let NERDTreeWinPos="left"
-let NERDTreeShowHidden=1
+let NERDTreeShowHidden=0
 let NERDTreeAutoDeleteBuffer=1
 let NERDTreeAutoDeleteBuffer=1
 
 " Colors
 colorscheme default
-
-" Mappings
-set nocompatible
 
 source ~/.vim/mappings.vim
 source ~/.vim/abbreviations.vim
@@ -61,3 +62,18 @@ set foldmethod=indent
 set foldlevel=99
 nnoremap ~ za
 
+highlight Comment ctermfg=darkgrey
+highlight Constant ctermfg=cyan
+highlight Identifier ctermfg=cyan
+highlight Statement ctermfg=blue
+highlight PreProc ctermfg=blue
+highlight Type ctermfg=blue
+highlight Special ctermfg=white
+highlight Underlined ctermfg=white
+highlight Ignore ctermfg=blue
+highlight Error ctermfg=white
+highlight LineNr ctermfg=237
+
+highlight LineNr ctermbg=232
+
+highlight Normal ctermfg=254
--- i3/battery.py
+++ i3/battery.py
@@ -5,7 +5,6 @@
 #
 # A battery indicator blocklet script for i3blocks
 
-import re
 from subprocess import check_output
 
 status = check_output(['acpi'], universal_newlines=True)
@@ -22,53 +21,32 @@ else:
     state_batteries=[]
     commasplitstatus_batteries=[]
     percentleft_batteries=[]
-    time = ""
     for battery in batteries:
         if battery!='':
             state_batteries.append(battery.split(": ")[1].split(", ")[0])
             commasplitstatus = battery.split(", ")
-            if not time:
-                time = commasplitstatus[-1].strip()
-                # check if it matches a time
-                time = re.match(r"(\d+):(\d+)", time)
-                if time:
-                    time = ":".join(time.groups())
-                    timeleft = " ({})".format(time)
-                else:
-                    timeleft = ""
-
-            p = int(commasplitstatus[1].rstrip("%\n"))
-            if p>0:
-                percentleft_batteries.append(p)
+            percentleft_batteries.append(int(commasplitstatus[1].rstrip("%\n")))
             commasplitstatus_batteries.append(commasplitstatus)
     state = state_batteries[0]
     commasplitstatus = commasplitstatus_batteries[0]
-    if percentleft_batteries:
-        percentleft = int(sum(percentleft_batteries)/len(percentleft_batteries))
-    else:
-        percentleft = 0
-
+    percentleft = int(sum(percentleft_batteries)/len(percentleft_batteries))
     # stands for charging
     FA_LIGHTNING = "<span color='yellow'><span font='FontAwesome'>\uf0e7</span></span>"
 
     # stands for plugged in
     FA_PLUG = "<span font='FontAwesome'>\uf1e6</span>"
 
-    # stands for using battery
-    FA_BATTERY = "<span font='FontAwesome'>\uf240</span>"
-
-    # stands for unknown status of battery
-    FA_QUESTION = "<span font='FontAwesome'>\uf128</span>"
-
+    fulltext = ""
+    timeleft = ""
 
     if state == "Discharging":
-        fulltext = FA_BATTERY + " "
+        time = commasplitstatus[-1].split()[0]
+        time = ":".join(time.split(":")[0:2])
+        timeleft = " ({})".format(time)
     elif state == "Full":
         fulltext = FA_PLUG + " "
-        timeleft = ""
     elif state == "Unknown":
-        fulltext = FA_QUESTION + " " + FA_BATTERY + " "
-        timeleft = ""
+        fulltext = "<span font='FontAwesome'>\uf128</span> "
     else:
         fulltext = FA_LIGHTNING + " " + FA_PLUG + " "
 
@@ -77,19 +55,19 @@ else:
             # exit code 33 will turn background red
             return "#FFFFFF"
         if percent < 20:
-            return "#FFFFFF"
+            return "#FF3300"
         if percent < 30:
-            return "#FFFFFF"
+            return "#FF6600"
         if percent < 40:
-            return "#FFFFFF"
+            return "#FF9900"
         if percent < 50:
-            return "#FFFFFF"
+            return "#FFCC00"
         if percent < 60:
-            return "#FFFFFF"
+            return "#FFFF00"
         if percent < 70:
-            return "#FFFFFF"
+            return "#FFFF33"
         if percent < 80:
-            return "#FFFFFF"
+            return "#FFFF66"
         return "#FFFFFF"
 
     form =  '<span color="{}">{}%</span>'
